336c8094ec2ca8d7e0ac6193d5a0fd40bb172ebf
Saidsadaoui <saiddidoubibou@gmail.com>
2025-08-11 00:16:11 +0100

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { NavBar } from './components/navBar'
 import { Home } from './sections/Home'
-import { Presentation } from './sections/Presentation'
+import { Presentation } from './sections/presentation'
 import { Services } from './sections/Services'
 import { Products } from './sections/Products'
 import { ContactUs } from './sections/contactUs'
